379ec2f3fe59c7ae4d1b126ba507d3061ca1bf45
sandric <kyrychenkoanton@gmail.com>
2016-03-13 16:33:25 +0200

    Refactoring

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,13 +1,12 @@
 class CommitsController < ApplicationController
 
 	def index
-		p params
-		keyboard = Keyboard.find(params[:keyboard_id])
-		@commits = keyboard.commits
+		@keyboard = Keyboard.find(params[:keyboard_id])
+		@commits = @keyboard.commits
 	end
 
 	def show
-		keyboard = Keyboard.find(params[:keyboard_id])
-		@commit = keyboard.commits.find(params[:id])
+		@keyboard = Keyboard.find(params[:keyboard_id])
+		@commit = @keyboard.commits.find(params[:id])
 	end
 end
